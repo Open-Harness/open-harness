a47d7f1f878401ea4f9f3abb719eacd33a14ab51
AbuUsama <AbuUsama@DAOresear.ch>
2026-01-16 18:16:54 +0000

    checkpoint(T001): Create add.ts file with exported add function

---
+++ packages/prd-workflow/add.ts
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers together and returns their sum
+ * @param a - The first number to add
+ * @param b - The second number to add
+ * @returns The sum of a and b
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
